8f74e57f74caa4169b9ef25832d714e97076d00d
WilkinsonK <keenanwilkinson@outlook.com>
2023-05-09 15:54:05 -0500

    remove pydantic and orm models

--- src/models/messages.py
+++
@@ -1,8 +0,0 @@
-from models.bases import HistoricalModel, VarCharField, UUIDField
-
-
-class MessageM(HistoricalModel):
-    id: UUIDField
-    ticket_id: UUIDField
-    owner_id: UUIDField
-    content: VarCharField(str, 512)
